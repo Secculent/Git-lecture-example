fdf48d644353ace6a971cc4d10be8422d10fd0d7
Mikolaj Karwowski <mikkarwowski@gmail.com>
2024-10-23 19:17:06 +0200

    Onboarding layouts

--- app/src/main/java/com/example/gitrepoapp/MainActivity.kt
+++ app/src/main/java/com/example/gitrepoapp/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Some important code on master
+
+        // Some code for onboarding layout maybe
     }
 }
